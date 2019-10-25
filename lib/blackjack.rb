f7f98836c51198f0ec5e66b718021684ffdc1698
Guligena Aierken <guligena_aierken@163.com>
2019-10-25 17:37:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,5 +64,10 @@ end
 
 def runner
   welcome
-code runner here
+  card_total = initial_round
+    until card_total > 21
+      card_total = hit?(card_total)
+      display_card_total(card_total)
+    end
+    end_game(card_total)
 end
